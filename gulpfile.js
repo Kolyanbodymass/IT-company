256d230864c28de4f12f9b40a99cb3524a7ec7fd
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-09-14 23:29:59 +0500

    tweaked the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 const del = require('del');
 const group_media = require('gulp-group-css-media-queries');
+const webp = require('gulp-webp');
+const webphtml = require('gulp-webp-html');
+const webpcss = require("gulp-webpcss");
+const webpack = require("webpack-stream");
 
 gulp.task('server', function() {
 
@@ -25,6 +29,7 @@ gulp.task('styles', function() {
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(group_media())
+        .pipe(webpcss())
         .pipe(gulp.dest("dist/css"))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
@@ -36,10 +41,13 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("./src/img/**/*.*", gulp.parallel('images'));
+    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
 });
 
 gulp.task('html', function() {
     return gulp.src("src/*.html")
+        .pipe(webphtml())
         .pipe(rename({suffix: '.not-compressed', prefix: ''}))
         .pipe(gulp.dest("dist/"))
         .pipe(htmlmin({ collapseWhitespace: true }))
@@ -61,11 +69,6 @@ gulp.task('fonts', function() {
         .pipe(gulp.dest("dist/fonts"));
 });
 
-gulp.task('icons', function() {
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"));
-});
-
 gulp.task('mailer', function() {
     return gulp.src("src/mailer/**/*")
         .pipe(gulp.dest("dist/mailer"));
@@ -73,10 +76,80 @@ gulp.task('mailer', function() {
 
 gulp.task('images', function() {
     return gulp.src("src/img/**/*")
-        .pipe(imagemin())
+        .pipe(webp({
+            quality: 70
+        }))
+        .pipe(gulp.dest("dist/img"))
+        .pipe(gulp.src("src/img/**/*"))
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true,
+            optimizationLevel: 3
+        }))
         .pipe(gulp.dest("dist/img"));
 });
 
-gulp.task("build", gulp.series('delDir', gulp.parallel('server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'mailer', 'images')));
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest('./dist/'))
+                .pipe(browserSync.stream());
+});
+
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'production',
+                    output: {
+                        filename: 'script.min.js'
+                    },
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task("build", gulp.series('delDir', gulp.parallel('server', 'styles', 'html', 'scripts', 'fonts', 'mailer', 'images'), "build-js"));
 gulp.task("default", gulp.parallel("watch", "build"));
 
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// Добавление класса с WEBP изображениями, если их поддерживает браузер
+
+function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function() {
+        callback(webP.height == 2);
+    };
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
+testWebP(function(support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    }
+});
